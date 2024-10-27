cffd6f4f14e531eafdd7e47a5ee037455f31eea3
DejanaGladic <dejanagladic@uns.ac.rs>
2024-10-27 23:57:43 +0100

    Initial VNIC and public IP address have been created

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 using System.Threading.Tasks;
 using Pulumi;
 using Pulumi.AzureNative;
-using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Network;
+using Pulumi.AzureNative.Resources;
 using NetworkInputs = Pulumi.AzureNative.Network.Inputs;
 
 class VMWithPrivateIPAddress : Stack
@@ -11,22 +11,23 @@ class VMWithPrivateIPAddress : Stack
     public VMWithPrivateIPAddress()
     {
         // Create an Azure Resource Group
-        // ResourceGroup() need name and args fo RG config 
+        // ResourceGroup() need name and args fo RG config
         // we can access RG name as resourceGroup.Name
         var resourceGroup = new ResourceGroup(
-            "VMResourceGroup",
+            "VM-Resource-Group",
             new ResourceGroupArgs
             {
-                ResourceGroupName = "VMResourceGroup",
-                Location = "WestUS" // but location is defined globally too but is recommended to set it for each resource
+                //ResourceGroupName = "VMResourceGroup", mislim da je nepotrebno
+                Location =
+                    "WestUS" // but location is defined globally too but is recommended to set it for each resource
+                ,
             }
         );
-
 
         // Create a virtual network - VNet
         // VirtualNetwork() use VNet name and args
         var virtualNetwork = new VirtualNetwork(
-            "VMvirtualNetwork",
+            "VM-Virtual-Network",
             new VirtualNetworkArgs()
             {
                 ResourceGroupName = resourceGroup.Name,
@@ -36,7 +37,7 @@ class VMWithPrivateIPAddress : Stack
                 {
                     // use CIDR notation - baseIPaddress/subnetMask
                     AddressPrefixes = new[] { "10.0.0.0/16" },
-                },                              
+                },
                 Subnets = new[]
                 {
                     // NetworkInputs class has to be specified because we have SubnetArgs() in different classes
@@ -44,15 +45,63 @@ class VMWithPrivateIPAddress : Stack
                     {
                         // first subnet has a address range from 10.0.0.0 to 10.0.0.254 (excluded)
                         AddressPrefix = "10.0.0.0/24",
-                        Name = "subnet-1-VMvirtualNetwork",
+                        Name = "VMVirtualNetwork-subnet-1",
                     },
-                }
+                },
+            }
+        );
+
+        // Create a public IP address for the VM and VNIC - separate from subnet - just for initial project
+        // going to be used in VNIC
+        var publicIp = new PublicIPAddress(
+            "VM-Public-Ip",
+            new()
+            {
+                ResourceGroupName = resourceGroup.Name,
+                PublicIPAllocationMethod = IPAllocationMethod.Dynamic
+               /*DnsSettings = new NetworkInputs.PublicIPAddressDnsSettingsArgs
+                {
+                    DomainNameLabel = domainNameLabel,
+                },*/
+            }
+        );
+
+        // Create VNIC (Virtual Network Interface Card)
+        var networkInterface = new NetworkInterface(
+            "VM-NIC",
+            new NetworkInterfaceArgs()
+            {
+                ResourceGroupName = resourceGroup.Name,
+                Location = "WestUS",
+                //DisableTcpStateTracking = true, ovo bas ne znam da li nam znaci ili ne
+                //EnableAcceleratedNetworking = true, ovo bas ne znam da li nam znaci ili ne
+                IpConfigurations = new[]
+                {
+                    new NetworkInputs.NetworkInterfaceIPConfigurationArgs
+                    {
+                        Name = "VM-ipconfig1",
+                        PublicIPAddress = new NetworkInputs.PublicIPAddressArgs
+                        {
+                            // dynamically assigned public IP address for access the VM from Internet - needs to be changed
+                            Id = publicIp.Id!
+                                
+                        },
+                        Subnet = new NetworkInputs.SubnetArgs
+                        {
+                            // define subnet id in which VNIC exists - subnet was created up in the code
+                            // get ID dinamically in code
+                            Id = virtualNetwork.Subnets.GetAt(0).Apply(subnet => subnet.Id!),
+                        },
+                        // dynamically assigned public IP address for access the VM from the same VNet - for secure communication inside the VNet
+                        // choose from subnet range (defined above)
+                        PrivateIPAllocationMethod = IPAllocationMethod.Dynamic,
+                    },
+                },
             }
         );
     }
 
- 
-    // Expose the Resource Group name as an output and can be used inside code - posle cemo to 
+    // Expose the Resource Group name as an output and can be used inside code - posle cemo to
     // this.ResourceGroupName = resourceGroup.Name;
     // Expose an output that contains the Resource Group name  - posle cemo to
     /*[Output]
