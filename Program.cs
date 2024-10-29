c958c41585d867e17565b46b39624020c821439b
DejanaGladic <dejanagladic@uns.ac.rs>
2024-10-29 17:52:55 +0100

    Region changed

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿// define stack resources
 using System.Threading.Tasks;
 using Pulumi;
-using Pulumi.AzureNative;
 using Pulumi.AzureNative.Network;
 using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Compute;
 using NetworkInputs = Pulumi.AzureNative.Network.Inputs;
+using ComputeInputs = Pulumi.AzureNative.Compute.Inputs;
 
 class VMWithPrivateIPAddress : Stack
 {
@@ -13,6 +14,7 @@ class VMWithPrivateIPAddress : Stack
         // Import the program's configuration settings.
         var config = new Pulumi.Config();
         var region = config.Get("azure-native:location") ?? "WestUS";
+        var vmSize = config.Get("vmSize") ?? "Standard_A1_v2";
 
         // Create an Azure Resource Group
         // ResourceGroup() need name and args fo RG config
@@ -111,7 +113,8 @@ class VMWithPrivateIPAddress : Stack
                 ResourceGroupName = resourceGroup.Name,
                 Location = region,
                 // NSG is associated with NIC - but maybe it is better to associate it with subnet?? because NSG on VNiC is hard to manage
-                NetworkSecurityGroup = new NetworkInputs.NetworkSecurityGroupArgs {
+                NetworkSecurityGroup = new NetworkInputs.NetworkSecurityGroupArgs
+                {
                     Id = securityGroup.Id
                 },
                 //DisableTcpStateTracking = true, ovo bas ne znam da li nam znaci ili ne
@@ -141,6 +144,60 @@ class VMWithPrivateIPAddress : Stack
                 }
             }
         );
+
+        // Create a VM
+       /* var vm = new VirtualMachine("VM-Azure-Pulumi", new()
+        {
+            ResourceGroupName = resourceGroup.Name,
+            NetworkProfile = new ComputeInputs.NetworkProfileArgs
+            {
+                NetworkInterfaces = new[] {
+                    new ComputeInputs.NetworkInterfaceReferenceArgs {
+                        Id = networkInterface.Id,
+                        Primary = true,
+                    },
+                },
+            },
+            HardwareProfile = new ComputeInputs.HardwareProfileArgs
+            {
+                VmSize = vmSize,
+            },
+            OsProfile = new AzureNative.Compute.Inputs.OSProfileArgs
+            {
+                ComputerName = vmName,
+                AdminUsername = adminUsername,
+                CustomData = Convert.ToBase64String(Encoding.UTF8.GetBytes(initScript)),
+                LinuxConfiguration = new AzureNative.Compute.Inputs.LinuxConfigurationArgs
+                {
+                    DisablePasswordAuthentication = true,
+                    Ssh = new AzureNative.Compute.Inputs.SshConfigurationArgs
+                    {
+                        PublicKeys = new[]
+                        {
+                        new AzureNative.Compute.Inputs.SshPublicKeyArgs {
+                            KeyData = sshKey.PublicKeyOpenssh,
+                            Path = $"/home/{adminUsername}/.ssh/authorized_keys",
+                        },
+                    },
+                    },
+                },
+            },
+            StorageProfile = new AzureNative.Compute.Inputs.StorageProfileArgs
+            {
+                OsDisk = new AzureNative.Compute.Inputs.OSDiskArgs
+                {
+                    Name = $"{vmName}-osdisk",
+                    CreateOption = AzureNative.Compute.DiskCreateOptionTypes.FromImage,
+                },
+                ImageReference = new AzureNative.Compute.Inputs.ImageReferenceArgs
+                {
+                    Publisher = osImagePublisher,
+                    Offer = osImageOffer,
+                    Sku = osImageSku,
+                    Version = osImageVersion,
+                },
+            },
+        });*/
     }
 
     // Expose the Resource Group name as an output and can be used inside code - posle cemo to
